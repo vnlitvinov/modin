e8c9f4320e1a53595045a24d395caa4f49201441
Vasilij Litvinov <vasilij.n.litvinov@intel.com>
2020-07-23 13:44:17 +0300

    Make caching more correct
    Signed-off-by: Vasilij Litvinov <vasilij.n.litvinov@intel.com>

--- modin/experimental/cloud/rpyc_proxy.py
+++ modin/experimental/cloud/rpyc_proxy.py
@@ -46,7 +46,7 @@ class WrappingConnection(rpyc.Connection):
         super().__init__(*a, **kw)
         self._remote_batch_loads = None
         self._remote_cls_cache = {}
-        self._static_cache = {}
+        self._static_cache = collections.defaultdict(dict)
         self._remote_dumps = None
         self._remote_tuplize = None
 
@@ -171,14 +171,14 @@ class WrappingConnection(rpyc.Connection):
                 key = handler
 
             if str(obj.____id_pack__[0]) in {'numpy', 'numpy.dtype'}:
-                try:
-                    cache = self._static_cache[obj.____id_pack__]
-                except KeyError:
-                    cache = self._static_cache[obj.____id_pack__] = {}
+                cache = self._static_cache[obj.____id_pack__]
                 try:
                     result = cache[key]
                 except KeyError:
                     result = cache[key] = super().sync_request(handler, *args)
+                    if handler == consts.HANDLE_GETATTR:
+                        # save an entry in our cache telling that we get this attribute cached
+                        self._static_cache[result.____id_pack__]['__getattr__'] = True
                 return result                    
 
         return super().sync_request(handler, *args)
@@ -187,10 +187,6 @@ class WrappingConnection(rpyc.Connection):
         if handler == consts.HANDLE_DEL:
             obj, _ = args
             if str(obj.____id_pack__[0]) in {'numpy', 'numpy.dtype'}:
-                """
-                # we have this cached, but a deletion is requested, remove the from cache
-                self._static_cache.pop(obj.____id_pack__, None)
-                """
                 if obj.____id_pack__ in self._static_cache:
                     # object is cached by us, so ignore the request or remote end dies and cache is suddenly stale;
                     # we shouldn't remove item from cache as it would reduce performance
